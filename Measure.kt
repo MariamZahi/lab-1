8a61383e0f40e980fafa90d65e7b4ab3b728442b
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2023-06-03 01:49:41 -0400

    Create km to mi

--- Measure.kt
+++ Measure.kt
@@ -1,3 +1,25 @@
 fun main(){
-    print("Hello")
+    //store conversion values 
+    val kmToMi = 0.621371
+    print("Enter value: ")
+
+        var stringInput = readLine()
+    //toDouble converts the Int to Decimal
+    val numericValue= stringInput!!.toDouble()
+    
+    print("Enter unit of measurement (km or mi)")
+    val unitOfMeasurement = readLine()
+
+    var result: Double = 0.0
+ 
+    if (unitOfMeasurement == "km"){
+        result = numericValue * kmToMi
+        print("$numericValue in $unitOfMeasurement is $result in miles")
+    } else if (unitOfMeasurement == "mi"){
+        result = numericValue / kmToMi
+        print("$numericValue in $unitOfMeasurement is $result in km")
+    } else ( 
+        print("$numericValue in $unitOfMeasurement is an unaccepted value")
+    )
+    
 }
